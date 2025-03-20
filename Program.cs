0a7a42727f08698871370d13b6e9e6d708c40449
NkosiphenduleJwara <38132434+NkosiphenduleJwara@users.noreply.github.com>
2025-03-20 14:58:48 +0200

    Learning about Git through datacamp for the Github Foundations examgit status!

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        int[] arrayOfIntegers = [1, 2, 3, 4]; // fixed, cannot say var
+
+        foreach (char character in "hello world") {
+            Console.WriteLine(character);
+        }
+
+        Console.WriteLine(arrayOfIntegers.Length); // get length of array
+
+        int[,] twoDimensionalArray = new int[2, 3]; // 2 rows, 3 columns
+        Console.WriteLine(twoDimensionalArray.GetLength(0)); // get number of rows
+        Console.WriteLine(twoDimensionalArray.GetLength(1)); // get number of columns
+
+        var employeeType = EmployeeType.Manager;
+        Console.WriteLine(employeeType); // Manager
+
+        // can cast enum to int
+        Console.WriteLine((int)employeeType); // 2
+        int employeeTwoType = (int) EmployeeType.Worker;
+        Console.WriteLine(employeeTwoType); // 8
+
+        
+    } 
+}
+
+enum EmployeeType // underlying type is int by default
+{
+    Manager = 2,
+    Supervisor = 4,
+    Worker = 8
+}
